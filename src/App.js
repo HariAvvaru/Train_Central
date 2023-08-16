82a5d3e32ba6fa993d2fafe6a5e70c2405eb7230
Avvaru Hari Sai Babu <95332303+HariAvvaru@users.noreply.github.com>
2023-08-16 20:36:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import Register from './Register';
 
+function App() {
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
